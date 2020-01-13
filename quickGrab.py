56e3eb0b010426d66ff8b09249d485ee30064125
Drew Seewald <atseewal@umich.edu>
2020-01-12 21:09:36 -0500

    Add docstring for SP2 coordinates

--- quickGrab.py
+++ quickGrab.py
@@ -5,6 +5,17 @@ Created on Sun Jan 12 19:04:50 2020
 @author: atseewal
 """
 
+"""
+Surface Pro, not docked
+-----------------------
+
+Cake Mania is launched in windowed mode and the window is moved into the top left corner with the top bar still showing
+Screen resolution is 1920x1080, scaling is set to 125%, and surface is not docked
+x_pad = 0
+y_pad = 32
+play area = x_pad, y_pad, x_pad+998, y_pad+750
+"""
+
 # Globals
 #--------
 x_pad = 0
